8fc567d7a1a7790bb10a3fe88d421419aaf1572e
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-07 13:09:48 +0200

    add sacafold for micropost alsow routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'applicatio#hello'
   get 'goodby' => 'application#goodby'
   resource :user
+  resource :microposts
 end
