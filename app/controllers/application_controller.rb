d7764a795ec0d4a80f3781a00c7a0806cf895e82
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-07 09:55:50 +0200

    add controller and routes with get action , add user scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Helloooo!"
+  end
+
+  def goodby
+    render html: "By by by"
+  end
 end
